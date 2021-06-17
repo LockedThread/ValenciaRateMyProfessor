3c5ba5dce6bbee0d3662e108321d8e148dbdc871
Warren Snipes <contact@warrensnipes.dev>
2021-06-17 02:08:18 -0400

    :building_construction: Cleaned up the code

--- main.go
+++ main.go
@@ -31,75 +31,71 @@ func ParseTable(data string) {
 	table := doc.Find("table")
 
 	table.Find("tr").Each(func(i int, rowSelection *goquery.Selection) {
-		fmt.Printf("\ni=%d", i)
 		if i < 2 {
 			return
 		}
 
-		var course model.Course
-
 		tableData := rowSelection.Find("td")
-
-		cont := true
-		tableData.Each(func(i int, selection *goquery.Selection) {
-			text := selection.Text()
-
-			if i == 0 {
-				if len(text) == 2 {
-					cont = false
-					return
-				} else {
-					cont = true
-				}
-			}
-			if cont == false {
-				return
-			}
-			//fmt.Printf("\ntext=%s", text)
-
-			switch i {
-			case 1:
-				course.Course = text
-				break
-			case 2:
-				course.Subject = text
-				break
-			case 3:
-				course.CRN = text
-				break
-			case 4:
-				break
-			case 5:
-				course.CampusID = text
-				break
-			case 6:
-				atoi, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					log.Fatal(err)
-				}
-				course.Credits = atoi
-				break
-			case 7:
-				course.Title = text
-				break
-			case 16:
-				course.Instructor = text
-				break
-			}
-		})
-
-		if len(course.Title) == 0 {
-			return
-		}
-
-		fmt.Printf("\ncourse=%s", course)
+		GetCourse(tableData)
 	})
 }
 
-func GetTableRow() {
+func GetCourse(tableData *goquery.Selection) (course model.Course) {
+	cont := true
+	tableData.Each(func(i int, selection *goquery.Selection) {
+		ParseTableRow(&course, &cont, i, selection)
+	})
 
+	if len(course.Title) == 0 {
+		return
+	}
+
+	fmt.Printf("\ncourse=%s", course)
+	return course
 }
 
-func ParseTableRow() {
+func ParseTableRow(course *model.Course, cont *bool, i int, selection *goquery.Selection) () {
+	text := selection.Text()
 
+	if i == 0 {
+		if len(text) == 2 {
+			*cont = false
+			return
+		} else {
+			*cont = true
+		}
+	}
+	if *cont == false {
+		return
+	}
+
+	switch i {
+	case 1:
+		course.Course = text
+		break
+	case 2:
+		course.Subject = text
+		break
+	case 3:
+		course.CRN = text
+		break
+	case 4:
+		break
+	case 5:
+		course.CampusID = text
+		break
+	case 6:
+		atoi, err := strconv.ParseFloat(text, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		course.Credits = atoi
+		break
+	case 7:
+		course.Title = text
+		break
+	case 16:
+		course.Instructor = text
+		break
+	}
 }
