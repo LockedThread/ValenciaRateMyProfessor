fa276ef495851d292c108e2c6a07da4ce16abc8a
Warren Snipes <contact@warrensnipes.dev>
2021-06-17 00:54:56 -0400

    :recycle: hmmmmm debugging a thought

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/LockedThread/ValenciaRateMyProfessor/model"
 	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
@@ -24,7 +26,55 @@ func main() {
 
 	// Find the review items
 	doc.Find("tr td").Each(func(i int, s *goquery.Selection) {
-		s.Nodes[0]
-		fmt.Printf("%s", s.Text())
+		if i%20 == 0 {
+			return
+		}
+
+		if s.HasClass("a") {
+			return
+		}
+
+		fmt.Printf("i=%d", i)
+		fmt.Printf("\nlength= %d", s.Length)
+		if s.Length() > 1 {
+			return
+		}
+
+		fmt.Printf("\ntext=%s", s.Text())
+
+		var attrCount = 0
+
+		find := s.Find("a")
+		if find != nil {
+			if len(find.Nodes) > 0 {
+				if len(find.Nodes[0].Attr) > 0 {
+					attrCount = len(find.Nodes[0].Attr)
+				}
+			}
+		}
+
+		fmt.Printf("\nattrCount=%d", attrCount)
+
+		if attrCount > 1 {
+			return
+		}
+
+		fmt.Printf("\n")
+		credits, err := strconv.Atoi(s.Nodes[5].Data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		course := model.Course{
+			CRN:        s.Nodes[0].Data,
+			Subject:    s.Nodes[1].Data,
+			Course:     s.Nodes[2].Data,
+			CampusID:   s.Nodes[4].Data,
+			Title:      s.Nodes[6].Data,
+			Credits:    credits,
+			Honors:     false,
+			Instructor: s.Nodes[15].Data,
+		}
+		fmt.Printf("%s", course)
 	})
 }
